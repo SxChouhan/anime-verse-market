c7c9af8c336af550e76c2fb75f85f6e8c3b377e3
Darkiesoul <siddharthmaheshchouhan@gmail.com>
2025-04-16 23:45:25 +0530

    Restored lovelable last commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
-import Pattern from '@/components/Pattern';
 import HeroSection from '@/components/home/HeroSection';
 import TrendingSection from '@/components/home/TrendingSection';
 import AnimeCategories from '@/components/home/AnimeCategories';
@@ -11,7 +10,6 @@ import ReviewsSection from '@/components/home/ReviewsSection';
 const Home = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Pattern />
       <Navbar />
       <HeroSection />
       <TrendingSection />
